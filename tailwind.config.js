b211cf832d01d856c8595e7be3c303695e2e6dae
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-29 00:25:52 +0600

    Some More Teaiwind CSS Utlity Classess Implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,8 +17,23 @@ export default {
          backgroundImage: (theme) => ({
             "gradiant-yellowred": "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
             "mobile-home": "url('./assets/HomePageGrapic.png')"
-         })
+         }),
+         fontFamily: {
+            dmsans: ["DM Sans", "sans-serif"],
+            montserrat: ["Montserrat", "sans-serif"]
+         },
+         content: {
+            evolvetext: "url('./assets/EvolveText.png')",
+            avstractwaves: "url('./assets/AvstractWaves.png')",
+            sparkles: "url('./assets/Sparkles.png')",
+            circles: "url('./assets/Circles.png')"
+         }
       },
+      screens: {
+         xs: "480px",
+         sm: "768px",
+         md: "1060px"
+      }
 	},
 	plugins: [],
 };
