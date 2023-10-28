4433d90e03fcd239088d2b50ba91599ce8275a95
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-29 03:07:44 +0600

    Hamber Icon Implemented For Mpbile Menu

--- src/sences/Navbar/index.tsx
+++ src/sences/Navbar/index.tsx
@@ -1,6 +1,8 @@
-import Link from "@/sences/Navbar/Link";
+import { useState } from "react";
 import Logo from "@/assets/Logo.png";
+import Link from "@/sences/Navbar/Link";
 import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 type Props = {
@@ -9,7 +11,10 @@ type Props = {
 };
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+	const [isMenuToggle, setIsMenuToggle] = useState<boolean>(false);
+
 	const flexBetween = "flex items-center justify-between";
+	const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
 	return (
 		<nav>
@@ -19,34 +24,43 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
 						{/* <==<<=== Left Side ===>>==> */}
 						<img src={Logo} alt="Logo" />
 						{/* <==<<=== Right Side ===>>==> */}
-						<div className={`${flexBetween} w-full`}>
-							<div className={`${flexBetween} gap-8 text-sm`}>
-								<Link
-									page="Home"
-									selectedPage={selectedPage}
-									setSelectedPage={setSelectedPage}
-								/>
-								<Link
-									page="Benefits"
-									selectedPage={selectedPage}
-									setSelectedPage={setSelectedPage}
-								/>
-								<Link
-									page="Our Classes"
-									selectedPage={selectedPage}
-									setSelectedPage={setSelectedPage}
-								/>
-								<Link
-									page="Contact Us"
-									selectedPage={selectedPage}
-									setSelectedPage={setSelectedPage}
-								/>
+						{isAboveMediumScreens ? (
+							<div className={`${flexBetween} w-full`}>
+								<div className={`${flexBetween} gap-8 text-sm`}>
+									<Link
+										page="Home"
+										selectedPage={selectedPage}
+										setSelectedPage={setSelectedPage}
+									/>
+									<Link
+										page="Benefits"
+										selectedPage={selectedPage}
+										setSelectedPage={setSelectedPage}
+									/>
+									<Link
+										page="Our Classes"
+										selectedPage={selectedPage}
+										setSelectedPage={setSelectedPage}
+									/>
+									<Link
+										page="Contact Us"
+										selectedPage={selectedPage}
+										setSelectedPage={setSelectedPage}
+									/>
+								</div>
+								<div className={`${flexBetween} gap-8`}>
+									<p>Sign In</p>
+									<button>Become a Member</button>
+								</div>
 							</div>
-							<div className={`${flexBetween} gap-8`}>
-								<p>Sign In</p>
-								<button>Become a Member</button>
-							</div>
-						</div>
+						) : (
+							<button
+								className="rounded-full bg-secondary-500 p-2"
+								onClick={() => setIsMenuToggle(!isMenuToggle)}
+							>
+								<Bars3Icon className="h-6 w-6 text-white" />
+							</button>
+						)}
 					</div>
 				</div>
 			</div>
