485c7d9a98bcb4c1634834fee95cd920385ffcf3
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-29 01:47:46 +0600

    Button Implemented

--- src/sences/Navbar/index.tsx
+++ src/sences/Navbar/index.tsx
@@ -10,21 +10,24 @@ const Navbar = (props: Props) => {
 		<nav>
 			<div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
 				<div className={`${flexBetween} mx-auto w-5/6`}>
-               <div  className={`${flexBetween} w-full gap-16`}>
-                  {/* <==<<=== Left Side ===>>==> */}
-                  <img src={Logo} alt="Logo" />
-                  {/* <==<<=== Right Side ===>>==> */}
-                  <div className={`${flexBetween} w-full`}>
-                     <div className={`${flexBetween} gap-8 text-sm`}>
-                        <p>Home</p>
-                        <p>Benefits</p>
-                        <p>Our Classes</p>
-                        <p>Contact Us</p>
-                     </div>
-                     
-                  </div>
-               </div>
-            </div>
+					<div className={`${flexBetween} w-full gap-16`}>
+						{/* <==<<=== Left Side ===>>==> */}
+						<img src={Logo} alt="Logo" />
+						{/* <==<<=== Right Side ===>>==> */}
+						<div className={`${flexBetween} w-full`}>
+							<div className={`${flexBetween} gap-8 text-sm`}>
+								<p>Home</p>
+								<p>Benefits</p>
+								<p>Our Classes</p>
+								<p>Contact Us</p>
+							</div>
+							<div className="">
+								<p>Sign In</p>
+								<button>Become a Member</button>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</nav>
 	);
