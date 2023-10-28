106817c73bd276bd551fe072fd3737cfc4e06edf
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-29 02:05:19 +0600

    Style Implemented On AnchorLink

---
+++ src/sences/Navbar/Link.tsx
@@ -0,0 +1,25 @@
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+type Props = {
+	page: string;
+	selectedPage: string;
+	setSelectedPage: (value: string) => void;
+};
+
+const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
+	const lowerCasePage = page.toLowerCase().replace(/ /g, "");
+
+	return (
+		<AnchorLink
+			href={`#${lowerCasePage}`}
+			className={`${
+				selectedPage === lowerCasePage ? "text-primary-500" : ""
+			} transition duration-500 hover:text-primary-300`}
+			onClick={() => setSelectedPage(lowerCasePage)}
+		>
+			{page}
+		</AnchorLink>
+	);
+};
+
+export default Link;
