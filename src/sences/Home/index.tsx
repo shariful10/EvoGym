0dfe802cd0b3faaead503e25dd317fbae7789d13
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-29 20:14:15 +0600

    Structure Setup On Home Page

--- src/sences/Home/index.tsx
+++ src/sences/Home/index.tsx
@@ -1,16 +1,32 @@
 import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
 import HomePageText from "@/assets/HomePageText.png";
-import HomePageGraphic from "@/assets/HomePageGraphic.png";
-import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
+import SponsorRedBull from "@/assets/SponsorRedBull.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
 
 type Props = {
 	setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Home = ({ setSelectedPage }: Props) => {
-	return <div>Home</div>;
+	const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+	return (
+		<section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+			{/* <==<<=== Main Header & Image ===>>==> */}
+			<div className="">
+				{/* <==<<=== Main Header ===>>==> */}
+				<div className="">
+               {/* <==<<=== Headeings ===>>==> */}
+               <div className=""></div>
+            </div>
+            {/* <==<<=== Image ===>>==> */}
+            <div className=""></div>
+			</div>
+		</section>
+	);
 };
 
 export default Home;
