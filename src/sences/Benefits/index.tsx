7c827fdd08f6df0604df9010c73e97488d14b7c7
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-29 22:48:59 +0600

    Header Section Implemented On Benefits Page

--- src/sences/Benefits/index.tsx
+++ src/sences/Benefits/index.tsx
@@ -5,6 +5,7 @@ import {
 } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
 import { SelectedPage } from "@/shared/types";
+import HText from "@/shared/HText";
 
 type Props = {
 	setSelectedPage: (value: SelectedPage) => void;
@@ -12,11 +13,17 @@ type Props = {
 
 const Benefits = ({ setSelectedPage }: Props) => {
 	return (
-		<section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+		<section id="benefits" className="mx-auto min-h-full w-5/6 py-20 scroll-smooth">
 			<motion.div
 				onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
 				className=""
-			></motion.div>
+			>
+            {/* <==<<=== Header ===>>==> */}
+            <div className="md:w-3/5 md:my-5">
+               <HText>more than just a gym</HText>
+               <p className="my-5 text-[18px]">We provide world class fitness equipment, trainers and classes to get you to your ultimate fitness goals with ease. We provide true care into each and every member.</p>
+            </div>
+         </motion.div>
 		</section>
 	);
 };
