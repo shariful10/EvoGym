ade71a590c648234f6f8ecd1bfb38107d4f346b9
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-29 23:08:02 +0600

    benifits Data Implemented For Benefits Page

--- src/sences/Benefits/index.tsx
+++ src/sences/Benefits/index.tsx
@@ -1,11 +1,32 @@
 import {
-	HomeModernIcon,
 	UserGroupIcon,
+	HomeModernIcon,
 	AcademicCapIcon,
 } from "@heroicons/react/24/solid";
+import HText from "@/shared/HText";
 import { motion } from "framer-motion";
 import { SelectedPage } from "@/shared/types";
-import HText from "@/shared/HText";
+
+const benifits: Array<BenefitType> = [
+	{
+		icon: <HomeModernIcon className="h-6 w-6" />,
+		title: "State of the Art of Facilities",
+		description:
+			"Neque adipiscing amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.",
+	},
+	{
+		icon: <UserGroupIcon className="h-6 w-6" />,
+		title: "State of the Art of Facilities",
+		description:
+			"Eu ipsum egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac tupic id.",
+	},
+	{
+		icon: <AcademicCapIcon className="h-6 w-6" />,
+		title: "Expert and Pro Trainers",
+		description:
+			"Fusce vestibulum aliquam ut cras. Nisl lectus egestas nisl. Lacus at mi sit pellentesque. Congue parturient",
+	},
+];
 
 type Props = {
 	setSelectedPage: (value: SelectedPage) => void;
@@ -13,17 +34,30 @@ type Props = {
 
 const Benefits = ({ setSelectedPage }: Props) => {
 	return (
-		<section id="benefits" className="mx-auto min-h-full w-5/6 py-20 scroll-smooth">
+		<section
+			id="benefits"
+			className="mx-auto min-h-full w-5/6 py-20 scroll-smooth"
+		>
 			<motion.div
 				onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
 				className=""
 			>
-            {/* <==<<=== Header ===>>==> */}
-            <div className="md:w-3/5 md:my-5">
-               <HText>more than just a gym</HText>
-               <p className="my-5 text-[18px]">We provide world class fitness equipment, trainers and classes to get you to your ultimate fitness goals with ease. We provide true care into each and every member.</p>
-            </div>
-         </motion.div>
+				{/* <==<<=== Header ===>>==> */}
+				<div className="md:w-3/5 md:my-5">
+					<HText>more than just a gym</HText>
+					<p className="my-5 text-[18px]">
+						We provide world class fitness equipment, trainers and classes
+						to get you to your ultimate fitness goals with ease. We
+						provide true care into each and every member.
+					</p>
+				</div>
+				{/* <==<<=== Benefits ===>>==> */}
+				<div className="md:flex items-center justify-between gap-8 mt-5">
+					{benifits.map((benifit) => (
+						<Benifit />
+					))}
+				</div>
+			</motion.div>
 		</section>
 	);
 };
