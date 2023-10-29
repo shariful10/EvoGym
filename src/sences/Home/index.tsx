317c164d81639765edea34a91e91eb41c643ee20
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-29 20:43:03 +0600

    Specific Height Implemented For Home Page Text & Image On Home Page

--- src/sences/Home/index.tsx
+++ src/sences/Home/index.tsx
@@ -18,7 +18,7 @@ const Home = ({ setSelectedPage }: Props) => {
 	return (
 		<section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
 			{/* <==<<=== Main Header & Image ===>>==> */}
-			<div className="md:flex mx-auto w-5/6 items-center justify-center">
+			<div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
 				{/* <==<<=== Main Header ===>>==> */}
 				<div className="">
 					{/* <==<<=== Headeings ===>>==> */}
