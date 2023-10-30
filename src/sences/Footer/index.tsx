0ccea74f8edafd61ae7f06abe927a311aff86d0c
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-31 02:14:23 +0600

    Description Implemented On Footer

--- src/sences/Footer/index.tsx
+++ src/sences/Footer/index.tsx
@@ -6,6 +6,12 @@ const Footer = () => {
 			<div className="justify-content mx-auto w-5/6 gap-16 md:flex">
 				<div className="mt-16 basis-1/2 md:mt-0">
 					<img alt="logo" src={Logo} />
+					<p className="my-5">
+						Lorem vitae ut augue auctor faucibus eget eget ut libero.
+						Elementum purus et arcu massa dictum condimentum. Augue
+						scelerisque iaculis orci ut habitant laoreet. Iaculis
+						tristique.
+					</p>
 				</div>
 			</div>
 		</footer>
