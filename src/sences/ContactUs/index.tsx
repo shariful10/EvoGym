73d214958b913e5dec708e17fcf4027da415cb20
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-31 01:53:14 +0600

    Input Field Style Implemented On Contact Us Page

--- src/sences/ContactUs/index.tsx
+++ src/sences/ContactUs/index.tsx
@@ -1,10 +1,13 @@
 import { SelectedPage } from "@/shared/types";
 
 type Props = {
-   setSelectedPage: (value: SelectedPage) => void;
+	setSelectedPage: (value: SelectedPage) => void;
 };
 
 const ContactUs = ({ setSelectedPage }: Props) => {
+	const inputStyles = `mb-5 w-full rounded-lg bg-primary-300
+  px-5 py-3 placeholder-white`;
+
 	return <div>ContactUs</div>;
 };
 
