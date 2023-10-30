46ae69ce5b4e4f074b534dcaff26e1e54a1711a5
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-30 20:25:40 +0600

    Description Implemented On Our Classes Section

--- src/sences/Benefits/index.tsx
+++ src/sences/Benefits/index.tsx
@@ -104,6 +104,33 @@ const Benefits = ({ setSelectedPage }: Props) => {
 							</div>
 						</div>
 						{/* <==<<=== Description ===>>==> */}
+						<motion.div
+							initial="hidden"
+							whileInView="visible"
+							viewport={{ once: true, amount: 0.5 }}
+							transition={{ delay: 0.2, duration: 0.5 }}
+							variants={{
+								hidden: { opacity: 0, x: 50 },
+								visible: { opacity: 1, x: 0 },
+							}}
+						>
+							<p className="my-5">
+								Nascetur aenean massa auctor tincidunt. Iaculis potenti
+								amet egestas ultrices consectetur adipiscing ultricies
+								enim. Pulvinar fames vitae vitae quis. Quis amet
+								vulputate tincidunt at in nulla nec. Consequat sed
+								facilisis dui sit egestas ultrices tellus. Ullamcorper
+								arcu id pretium sapien proin integer nisl. Felis orci
+								diam odio.
+							</p>
+							<p className="mb-5">
+								Fringilla a sed at suspendisse ut enim volutpat. Rhoncus
+								vel est tellus quam porttitor. Mauris velit euismod
+								elementum arcu neque facilisi. Amet semper tortor
+								facilisis metus nibh. Rhoncus sit enim mattis odio in
+								risus nunc.
+							</p>
+						</motion.div>
 						{/* <==<<=== Button ===>>==> */}
 					</div>
 				</div>
