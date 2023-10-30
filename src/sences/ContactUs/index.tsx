6a6be1b64e6b0a720c173ca22334f501ac682fe1
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-31 03:19:42 +0600

    Final Commit

--- src/sences/ContactUs/index.tsx
+++ src/sences/ContactUs/index.tsx
@@ -69,7 +69,9 @@ const ContactUs = ({ setSelectedPage }: Props) => {
 						<form
 							target="_blank"
 							onSubmit={onSubmit}
-							action="https://formsubmit.co/e8a5bdfa807605332f809e5656e27c6e"
+							action={`https://formsubmit.co/${
+								import.meta.env.VITE_EMAIL
+							}`}
 							method="POST"
 						>
 							<input
